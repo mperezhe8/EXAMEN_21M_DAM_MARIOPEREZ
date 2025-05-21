63013f6dd4075b2a36720953ef8de551412cd6c3
perez <perez@CALCHAKI>
2025-05-21 11:32:19 +0200

    drg

---
+++ src/examen/PrimoMario.java
@@ -0,0 +1,12 @@
+package examen;
+
+import java.util.Scanner;
+
+public class PrimoMario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+        System.out.print("Introduce un número para averiguar si es primo o no: ");
+        int numero = sc.nextInt();
+    }
+}
